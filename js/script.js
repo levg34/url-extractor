bbeb12135f036dce4822b2e19c7d33eb0b8347a4
levg34 <levg34@users.noreply.github.com>
2017-03-31 17:26:42 +0200

    select parameters

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var uri = ''
+var params = ''
 
 function getUrl(uri) {
 	var uri_enc = uri.split('?u=')[1].split('&h=')[0]
@@ -11,12 +13,30 @@ var uri = 'https://lm.facebook.com/l.php?u=http%3A%2F%2Fbbc.in%2F2nBsz8v&h=ATNn3
 console.log(getUrl(uri))
 */
 
+function toggleParam(param) {
+	// if $('#cbox1').checked ...
+	var durl = uri+'?'+param+'='+params[param]
+	params[param]
+	$('#d_url').html('<a href="'+durl+'">'+durl+'</a>')
+}
+
 function pressKey(e) {
 	if (e.key=='Enter') {
-		var uri = document.querySelector('#f_url').value
-		document.querySelector('#d_url').innerHTML = '<a href="'+getUrl(uri)+'">'+getUrl(uri)+'</a>'
-		console.log()
+		uri = $('#f_url').val()
+		uri = getUrl(uri)
+		
+		var parser = purl(uri)
+		
+		uri = parser.attr('protocol')+'://'+parser.attr('host')+parser.attr('path')
+		params = parser.param()
+		
+		for(var param in params) {
+			$('#params').append('<input type="checkbox" id="'+param+'" onclick="toggleParam(\''+param+'\')">'+param+': '+params[param]+'<br>')
+		}
+		
+		$('#d_url').html('<a href="'+uri+'">'+uri+'</a>')
 	}
 }
 
 // https://lm.facebook.com/l.php?u=https%3A%2F%2Fwww.change.org%2Fp%2Fanne-hidalgo-n-ayons-pas-peur-refusons-de-murer-la-tour-eiffel%3Frecruiter%3D703009373%26utm_source%3Dshare_petition%26utm_medium%3Dfacebook%26utm_campaign%3Dautopublish%26utm_term%3Dmob-xs-share_petition-no_msg&h=ATNkGiPffPBTXVynRs7AU5CoUuDaUm9KrMk9sAZA9ymqDE0c8jWG63nkshvhO8h1TgA_6RE6PG9hxQn_YNAGkUz-JHJUeLUUXr_CHEvuYJ257wAKy0EQUAtJ_8Ks6C0cSmJOew&enc=AZM0FnKgWfRhigFzrnzTN2I38noONcafDEIYJlqPYDYThPZCgfN5aLDQ2akQ6lKaV7mOYWv2LY28-CQoNiwHUV89Nm546XM42_rhVinzA3D1F8CbRuZTzbEZz2hmOIVbu3mWTyeibAZK_FRXPSUkLJHD3g1_0EbiR7GrAP-UbfF96qnZbtgm1PuyT_e46fxX-7jUsMW36MqgIRygahmTwno0mTDDCuTaqyR0omY7QoE-Q6n7wl_ECt4Xkk3sdue-xl16j0SeTrFCpm-RrSo0B65LtMS69_XFz_iQqQO8zykVpKb5O1HJJk9maZI1dUkZmbo&s=1
+
